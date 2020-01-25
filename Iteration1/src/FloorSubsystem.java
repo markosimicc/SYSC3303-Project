0b73b77a47ac7865d8e002a738737a86763943a6
markosimicc <markosimic@cmail.carleton.ca>
2020-01-25 17:50:20 -0500

    Intial commit

---
+++ Iteration1/src/FloorSubsystem.java
@@ -0,0 +1,13 @@
+
+public class FloorSubsystem implements Runnable {
+	private int direction;
+	private String time = null;
+	private int floorNum;
+
+	@Override
+	public void run() {
+		
+		
+	}
+
+}
---
+++ Iteration1/src/Tst.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Marko
+ *
+ */
+public class Tst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
