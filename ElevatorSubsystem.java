bbe0556e351d832d998797fc7aac7dfb8fa85d3b
architbhatia14 <55498272+architbhatia14@users.noreply.github.com>
2020-03-05 15:42:16 -0500

    it3

--- ElevatorSubsystem.java
+++ ElevatorSubsystem.java
@@ -1,137 +1,169 @@
-package scheduler_state;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
-import static scheduler_state.Transition.Door_Opening;
-import static scheduler_state.Transition.Reach_Floor;
-import static scheduler_state.Transition.Start_Motor;
-import static scheduler_state.Transition.Door_closing;
-
-
+/**
+ * 
+ * @author Archit Bhatia(states)
+ *
+ * 
+ * 
+ *         a list of the transitions of the Schedulers states
+ * 
+ */
 public class ElevatorSubsystem implements Runnable {
-
 	private int elevatorNum; // Declaration of class fields
+	private int currFloor = 1;
 	private Scheduler scheduler;
 	private RequestInfo info;
-
 	// private static Logger LOGGER = Logger.getLogger(Scheduler.class.getName());
-	public static String direction = " ";
+	public ElevState stat = ElevState.WAITING;
+	public ElevatorTransistions trans;
+
+	DatagramPacket sendPacket, receivePacket;
+	DatagramSocket sendSocket, receiveSocket;
+	private int port = 69;
+	private byte[] responseMessage = new byte[100];
 
 	/**
+	 * 
 	 * Constructor for the Elevator Subsystem class
 	 * 
+	 * 
+	 * 
 	 * @param elevatorNum
-	 * @param s
+	 * 
+	 * 
+	 * 
 	 */
+
 	public ElevatorSubsystem(int elevatorNum, Scheduler s) {
 		this.elevatorNum = elevatorNum;
 		this.scheduler = s;
+
+		try {
+			sendSocket = new DatagramSocket();
+			receiveSocket = new DatagramSocket(port);
+		} catch (SocketException se) {
+			se.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public int getFloor() {
+
+		return currFloor;
+	}
+
+	public void setFloor(int x) {
+
+		currFloor = x;
+	}
+
+	public void receiveAndEcho() {
+
+		//while (scheduler.getRequest()) {
+
+			byte data[] = new byte[1];
+			data[0] = (byte) currFloor;
+			try {
+				sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), port);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				sendSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			byte dataR[] = new byte[1];
+			receivePacket = new DatagramPacket(dataR, dataR.length);
+
+			try {
+				receiveSocket.receive(receivePacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			byte temp[] = receivePacket.getData();
+			currFloor = temp[0];
+		   //  }
 	}
 
 	/**
+	 * 
 	 * run() receives the information from the scheduler, prints out a message and
+	 * 
 	 * sends the information back using an instance of scheduler
+	 * 
 	 */
 
-	public static enum State {
-
-		Start {
-
-		@Override
-			public State next(Transition transition) {
-			
-				if (transition == Door_closing) {
-					return Door_Closed;		
-				
-				}
-				
-				return ILLEGAL;
-					
+	public enum ElevState {
+		WAITING {
+			@Override
+			public ElevState next(ElevatorTransistions transition) {
+				return (transition == ElevatorTransistions.Door_closing) ? Door_Closed : ILLEGAL;
 			}
 		},
 
 		Door_Closed {
-
 			@Override
-			public State next(Transition transition) {
+			public ElevState next(ElevatorTransistions transition) {
 
-				if((transition == Start_Motor)){
-					return Moving;
-				}
-					return ILLEGAL;
-				
+				return (transition == ElevatorTransistions.Start_Motor) ? Moving : ILLEGAL;
 			}
 		},
 
 		Moving {
-
 			@Override
-			public State next(Transition transition) {
-
-				if(transition == Reach_Floor){
-					return Stopped;
-				}
-				return ILLEGAL;
-
+			public ElevState next(ElevatorTransistions transition) {
+				return (transition == ElevatorTransistions.Reach_Floor) ? Stopped : ILLEGAL;
 			}
-
 		},
 
 		Stopped {
 			@Override
-			public State next(Transition transition) {
-				if(transition == Door_Opening){
-					return Start;
-				}
-				
-				return ILLEGAL;
+			public ElevState next(ElevatorTransistions transition) {
+				return (transition == ElevatorTransistions.Door_Opening) ? WAITING : ILLEGAL;
 			}
 		},
 
 		ILLEGAL {
-
 			@Override
-			public State next(Transition transition) {
-
+			public ElevState next(ElevatorTransistions transition) {
 				return ILLEGAL;
-
 			}
-
 		};
-
-		public State next(Transition transition) {
-
+		public ElevState next(ElevatorTransistions transition) {
+			// TODO Auto-generated method stub
 			return null;
-
 		}
 
 	}
 
-	/*
-	 * public static void main(String[] args) {
-	 * 
-	 * State finish = run(State.Start, Down_Request, Up_Request, Start_Motor,
-	 * Reach_Floor, Door_Opening, Awaiting_Next_Destination);
-	 * 
-	 * }
-	 * 
-	 * public static State run(State start, Transition... transitions) {
-	 * 
-	 * State state = start; LOGGER.log(Level.INFO, "start state: {0}", start);
-	 * 
-	 * for (Transition transition : transitions) { state = state.next(transition);
-	 * LOGGER.log(Level.INFO, "current state: {0}", state); }
-	 * 
-	 * LOGGER.info("finished");
-	 * 
-	 * return state;
-	 * 
-	 * }
-	 */
-	public void run() {
-		info = scheduler.recieveInfo(false);
-		System.out.println("Elevator Recieved: " + info.time + " " + info.floor + " " + info.direction + " " + info.elevator);
-		System.out.println("Elevator " + this.elevatorNum + " is ready to move.");
-		scheduler.sendInfo(info);
-
+	public ElevState getState(ElevState st) {
+		return st;
 	}
 
+	public void run() {
+		while (true) {
+			info = scheduler.recieveInfo(false);
+			System.out.println(
+					"Elevator Recieved: " + info.time + " " + info.floor + " " + info.direction + " " + info.elevator);
+			stat = stat.next(ElevatorTransistions.Door_closing);
+			System.out.println("Elevator is in state: " + stat);
+			System.out.println("Elevator " + this.elevatorNum + " is ready to move.");
+			stat = stat.next(ElevatorTransistions.Start_Motor);
+			System.out.println("Elevator is in state: " + stat);
+			scheduler.sendInfo(info);
+			stat = stat.next(ElevatorTransistions.Reach_Floor);
+			System.out.println("Elevator is in state: " + stat);
+			stat = stat.next(ElevatorTransistions.Door_Opening);
+		}
+	}
 }
