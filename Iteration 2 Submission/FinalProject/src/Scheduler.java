f2b084945583b1b776f540760be39d7833b900e2
markosimicc <markosimic@cmail.carleton.ca>
2020-03-05 21:18:54 -0500

    Starting Iteration 3

--- Iteration 2 Submission/FinalProject/src/Scheduler.java
+++ Iteration 2 Submission/FinalProject/src/Scheduler.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.*;
+
 /*
  * Scheduler receives information from FloorSubsystem and passes it to ElevatorSubsystem and vice versa.
  * @author Marko Simic and Michael Slokar (states)
@@ -5,17 +12,30 @@
  */
 
 public class Scheduler implements Runnable {
-
+	DatagramPacket sendPacket, receivePacket;
+	DatagramSocket sendReceiveSocket, floorSocket, replySocket,elevatorSocket;
 	public boolean reply = true;
-
+	public Queue<String> upQueue = new LinkedList<>();
+	public Queue<String> downQueue = new LinkedList<>();
+	public InetAddress floorIp;
+	public int floorport;
 	public SchedulerTransistions transition;
-
+	private int numElevators = 4;
+	public ArrayList<Integer> elevfloors = new ArrayList<>();
 	public State st = State.WAITING;
 
 	public RequestInfo str;
 
 	public Scheduler() {
-
+		try {
+			floorSocket = new DatagramSocket(23);
+			sendReceiveSocket = new DatagramSocket();
+			replySocket = new DatagramSocket();
+			elevatorSocket = new DatagramSocket(5050);
+		}catch (SocketException se) {   // Can't create the socket.
+			se.printStackTrace();
+			System.exit(1);
+		}
 		System.out.println("Scheduler Initial State : " + getState(st));
 
 	}
@@ -23,14 +43,104 @@ public class Scheduler implements Runnable {
 	@Override
 
 	public void run() {
+		byte data[] = new byte[2];
+		receivePacket = new DatagramPacket(data, data.length);
+		try {
+			floorSocket.receive(receivePacket);
+			} 
+		catch (IOException e) {
+			//System.out.print("IO Exception: likely:");
+			//System.out.println("Receive Socket Timed Out.\n" + e);
+			e.printStackTrace();
+			System.exit(1);
+			}
+		System.out.println("Scheduler is gonna receive " + data.toString() + "number of requests");
+		floorIp = receivePacket.getAddress();
+		floorport = receivePacket.getPort();
+		String s = "Ready for Requests";
+		byte[] send = s.getBytes();
+		sendPacket = new DatagramPacket(send,send.length,floorIp,floorport);
+		try {
+			replySocket.send(sendPacket);
+			}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+			}
+		for(int i = 0; i< data[0];i++) {
+			receiveRequest();
+		}
+		for(int i = 0; i <numElevators ; i++) {
+			int elevFloor = getElevNumber(23+i);
+			elevfloors.add(elevFloor);
+		}
+		schedule();
+		
+		
 
 	}
+	public void schedule() {
+		
+	}
+	public int getElevNumber(int port) {
+		String elev = "What floor";
+		byte[] elevreq = elev.getBytes();
+		sendPacket = new DatagramPacket(elevreq,elevreq.length,floorIp,port);
+		try {
+			sendReceiveSocket.send(sendPacket);
+			}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+			}
+		byte[] reply = new byte[4];
+		receivePacket = new DatagramPacket(reply,reply.length);
+		try {
+			sendReceiveSocket.receive(sendPacket);
+			}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+			}
+		int a = reply[0];
+		return a;
 
+	}
+	public void receiveRequest() {
+		byte data[] = new byte[50];
+		receivePacket = new DatagramPacket(data, data.length);
+		try {
+			floorSocket.receive(receivePacket);
+			} 
+		catch (IOException e) {
+			//System.out.print("IO Exception: likely:");
+			//System.out.println("Receive Socket Timed Out.\n" + e);
+			e.printStackTrace();
+			System.exit(1);
+			}
+		String s = "Next";
+		byte[] send = s.getBytes();
+		sendPacket = new DatagramPacket(send,send.length,floorIp,floorport);
+		try {
+			replySocket.send(sendPacket);
+			}
+		catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+			}
+		System.out.println("Scheduler received : " + data.toString());
+		parse(data);
+		
+	}
 	/**
 	 * Takes in the information passed as an argument and notifies the system
 	 * @param info
 	 */
-
+	public void parse(byte[] msg) {
+		String req= "";
+		
+		
+	}
 	public synchronized void sendInfo(RequestInfo info) {
 
 		while (!(str == null)) {
@@ -38,27 +148,18 @@ public class Scheduler implements Runnable {
 			try {
 
 				wait();
-
 			}
-
 			catch (InterruptedException e) {
-
 				return;
-
 			}
-
 		}
-
 		if (Thread.currentThread().getName().contains("Floor")) {
 
 			reply = false;
-
 		}
-
 		else {
 
 			reply = true;
-
 		}
 
 		str = info;
